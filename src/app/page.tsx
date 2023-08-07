4f6d96c0fffbc764fa9d7d662edb22d3d00fdc98
hayato94087 <hayato94087@gmail.com>
2023-08-07 10:21:20 +0900

    ページにフォームを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import ContactForm from "@/components/contact-form";
+
 export default function Home() {
   return (
-    <main className="text-lg">
-      テストページ
+    <main className="container relative mx-auto">
+      <div className="mx-auto lg:w-[800px] py-10 px-10">
+        <h1 className="text-2xl font-bold text-center pb-10">お問い合わせ</h1>
+        <ContactForm />
+      </div>
     </main>
-  )
+  );
 }
